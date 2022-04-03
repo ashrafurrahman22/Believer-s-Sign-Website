24dc64b682866ebe8a94cdd4ea78234acb3ca5f2
ashrafur22 <ashrafurrahman22@gmail.com>
2022-04-03 22:19:44 +0600

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Review from './Components/Review/Review';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
+import PageNotFound from './Components/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h2>helllo</h2>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/review' element={<Review></Review>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+  <Container>
+    <div>
+    <Navbar.Brand className='navName'>Believer's Sign</Navbar.Brand>
+    </div>
+    <div>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link><Link className='navbar' to='/'>Home</Link></Nav.Link>
+        <Nav.Link><Link className='navbar' to='/review'>Review</Link></Nav.Link>
+        <Nav.Link><Link className='navbar' to='/dashboard'>Dashboard</Link></Nav.Link>
+        <Nav.Link><Link className='navbar' to='/blogs'>Blogs</Link></Nav.Link>
+        <Nav.Link><Link className='navbar' to='/about'>About</Link></Nav.Link>
+       
+      </Nav>
+    </Navbar.Collapse>
+    </div>
+  </Container>
+</Navbar>
+        </div>
+    );
+};
+
+export default Header;
