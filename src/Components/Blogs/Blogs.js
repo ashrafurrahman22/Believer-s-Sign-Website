1e06e627cb98a85f8cbc47d67379b6892eab8d08
ashrafur22 <ashrafurrahman22@gmail.com>
2022-04-03 22:45:58 +0600

    blogs-updated

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
 
 const Blogs = () => {
     return (
         <div>
-            <h2>this is blogs</h2>
+            <div>
+            <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    What is Context Api
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    
+    <p>this is a library.</p>
+  </Dropdown.Menu>
+</Dropdown>
+            </div>
+            <div>
+            <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  What is Semantic tag?
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    
+    <p>this is a library.</p>
+  </Dropdown.Menu>
+</Dropdown>
+            </div>
+
+            <div>
+            <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    What is the differences between inline and block elements?
+  </Dropdown.Toggle>
+  <Dropdown.Menu>
+    
+    <p>this is a library.</p>
+  </Dropdown.Menu>
+</Dropdown>
+            </div>
         </div>
     );
 };
