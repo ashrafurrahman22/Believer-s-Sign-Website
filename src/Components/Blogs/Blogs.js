0a2b4993e8da5c2ed69678f504afa1db14a54e1c
ashrafur22 <ashrafurrahman22@gmail.com>
2022-04-04 09:48:26 +0600

    blogs-pageNotFound-updated

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -6,35 +6,38 @@ const Blogs = () => {
         <div>
             <div>
             <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  <Dropdown.Toggle variant="success w-50 my-3 mx-3" id="dropdown-basic">
     What is Context Api
   </Dropdown.Toggle>
-  <Dropdown.Menu>
+  <Dropdown.Menu variant='mx-3 my-3 p-5'>
     
-    <p>this is a library.</p>
+    <p>Context Api is an alternative of Props drilling. If we need to send data from grandparents to child components then we need to do props drilling. But props drilling isn't a good way. Because of that we have another solution called Context Api. We can send single or multiple data to any componenets by context Api. Context Api made developer life easier.</p>
   </Dropdown.Menu>
 </Dropdown>
             </div>
             <div>
             <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  <Dropdown.Toggle variant="success w-50 my-3 mx-3" id="dropdown-basic">
   What is Semantic tag?
   </Dropdown.Toggle>
-  <Dropdown.Menu>
+  <Dropdown.Menu variant='mx-3 my-3 p-5'>
     
-    <p>this is a library.</p>
+    <p>A semantic tag clearly describes its meaning. Let's start with examples, in html the div tag tells nothing about its content, but an article tag has a specifik meaning. we know that article tag refers that there would be an article. But div tag tell us nothing. Here is the difference. Semantic tag will tell us whats their work in html.</p>
   </Dropdown.Menu>
 </Dropdown>
             </div>
 
             <div>
             <Dropdown>
-  <Dropdown.Toggle variant="success" id="dropdown-basic">
+  <Dropdown.Toggle variant="success w-50 my-3 mx-3" id="dropdown-basic">
     What is the differences between inline and block elements?
   </Dropdown.Toggle>
-  <Dropdown.Menu>
+  <Dropdown.Menu variant='mx-3 my-3 p-5'>
     
-    <p>this is a library.</p>
+    <p>Every HTML element has a default display value, depending on what type of element it is.</p>
+    <p>There are two display values: block and inline</p>
+    <p>A block-level element always starts on a new line. It always takes up the full width available.</p>
+    <p>On the other hand, An inline element does not start on a new line. It only takes up as much width as necessary.</p>
   </Dropdown.Menu>
 </Dropdown>
             </div>
--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './PageNotFound.css'
 
 const PageNotFound = () => {
     return (
-        <div>
-            <h3>Page Not Found</h3>
+        <div className='styles'>
             <h1>404</h1>
+            <h4>Sorry!! Page Not Found</h4>
         </div>
     );
 };
